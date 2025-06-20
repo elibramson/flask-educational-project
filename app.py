99ece01494baa172726e83afec87f37e45dc864f
Eli Bramson <eli.bramson@gmail.com>
2025-06-20 10:15:39 +0200

    feat(docker): add containerization support

--- app.py
+++ app.py
@@ -54,4 +54,8 @@ if __name__ == "__main__":
     # Create the instance folder and database
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    port = int(os.environ.get('PORT', 5000))
+    
+    # Run the app
+    app.run(host='0.0.0.0', port=port, debug=False)
